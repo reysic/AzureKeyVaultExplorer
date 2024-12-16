faeadbfb8dd99b18e40986fcaf79c015b968dd5b
reysic <36483043+reysic@users.noreply.github.com>
2024-12-16 13:42:16 -0600

    adjust package handling for vs2017 compatibility

--- .init/init.ps1
+++ .init/init.ps1
@@ -16,7 +16,7 @@ $nuGetExe = (Join-Path $PSScriptRoot nuget\nuget.exe)
 $nuGetCfg = (Join-Path $PSScriptRoot nuget\nuget.config)
 
 # NuGet (restored) packages root
-$nuGetPackageRoot = Join-Path $PSScriptRoot '..\.packages'
+$nuGetPackageRoot = Join-Path $PSScriptRoot '..\packages'
 
 if (-not (Test-Path $nuGetPackageRoot -PathType Container))
 {
@@ -118,7 +118,7 @@ function New-NuGetPackagePropertyFile(
         $xw.WriteStartElement("PropertyGroup", $ns);
         
         $xw.WriteComment(" Packages root ");
-        $xw.WriteElementString("PackagesRoot", $ns, '$(RepositoryRoot)\.packages');
+        $xw.WriteElementString("PackagesRoot", $ns, '$(RepositoryRoot)\packages');
         $xw.WriteComment(" Packages sorted by name and version ");
     }
 
