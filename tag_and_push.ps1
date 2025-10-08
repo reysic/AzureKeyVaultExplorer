a45958ddc9e7036d5ff5aab45261dc47774029e2
reysic <36483043+reysic@users.noreply.github.com>
2025-10-08 16:11:04 -0500

    cleanup following update incorporation

--- tag_and_push.ps1
+++ tag_and_push.ps1
@@ -1,5 +1,10 @@
-# Run tag.ps1 to create a new tag
-. "$PSScriptRoot\tag.ps1"
+# Create a new tag
+$gitOutput = $(git log -1 --pretty=format:"%h;%cd" --date=iso)
+$commitIdDate = $gitOutput.Split(';')
+$shortCommitId = $gitOutput[0]
+$commitDateUTC = [datetime]::Parse($commitIdDate[1]).ToUniversalTime()
+$versionString = $commitDateUTC.ToString('vyyyy.MMdd.HHmm.ss')
+git tag $versionString
 
 # Get the latest tag name
 $tagName = git describe --tags
